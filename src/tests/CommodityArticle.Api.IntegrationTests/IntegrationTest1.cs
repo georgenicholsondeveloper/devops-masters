ffbe6118a285dfa62f2e10866f42ab778ba1d629
George Nicholson <43937454+georgenicholsondeveloper@users.noreply.github.com>
2026-03-12 18:40:44 +0000

    Update IntegrationTest1.cs

--- src/tests/CommodityArticle.Api.IntegrationTests/IntegrationTest1.cs
+++ src/tests/CommodityArticle.Api.IntegrationTests/IntegrationTest1.cs
@@ -8,7 +8,7 @@ public class HealthEndpointTests
     public async Task GET_health_returns_200_and_expected_payload()
     {
         // Arrange
-        var _client = new HttpClient { BaseAddress = new Uri("https://localhost:8080") };
+        var _client = new HttpClient { BaseAddress = new Uri("http://localhost:8080") };
 
         // Act
         var response = await _client.GetAsync("/weatherforecast");
