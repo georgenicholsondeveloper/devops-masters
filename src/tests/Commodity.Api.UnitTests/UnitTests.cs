453c25b69c48a89574df26689d3f4e285ad96476
George Nicholson <43937454+georgenicholsondeveloper@users.noreply.github.com>
2026-04-06 18:25:52 +0000

    fix test

--- src/tests/Commodity.Api.UnitTests/UnitTests.cs
+++ src/tests/Commodity.Api.UnitTests/UnitTests.cs
@@ -16,6 +16,35 @@ namespace Commodity.Api.UnitTests
             _commodityServiceMock = new Mock<ICommodityService>();
         }
 
+         [Test]
+        public async Task GetCommodityById_ShouldReturn200_AndExpectedCommodity()
+        {
+            // Arrange
+
+            var expectedCommodity = new CommodityModel { Id = 1, Name = "Test Article 1", Price = 10.0m, Category = "Test Category", CreatedAt = DateTime.UtcNow, UpdatedAt = DateTime.UtcNow };
+
+            _commodityServiceMock.Setup(x => x.GetCommodityByIdAsync(It.IsAny<int>())).ReturnsAsync(expectedCommodity);
+
+            var sut = new CommodityController(_commodityServiceMock.Object);
+
+            //Act
+
+            var response = await sut.GetCommodity(1);
+
+            var actionResult = response.Result as OkObjectResult;
+
+            //Assert
+
+            Assert.That(actionResult, Is.Not.Null);
+
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(actionResult.StatusCode, Is.EqualTo(200));
+                Assert.That(actionResult.Value, Is.InstanceOf<CommodityModel>());
+                //Assert.That(actionResult.Value as CommodityModel, Is.EquivalentTo(expectedCommodity));
+            }
+        }
+
         [Test]
         public async Task GetAllCommodities_ShouldReturn200_AndContainAllCommodities()
         {
