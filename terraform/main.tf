fbb33d7fc5dfcd330da9f2c658972bfbab852a3b
George Nicholson <43937454+georgenicholsondeveloper@users.noreply.github.com>
2026-04-16 15:16:02 +0000

    Add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-d-commodity-api"
+  location = "West Europe"
+
+  tags = {
+    Environment = "Development"
+    Project     = "Commodity Api"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "subscription_id" {
+  description = "The Subscription ID for the Azure resources."
+  type        = string
+}
